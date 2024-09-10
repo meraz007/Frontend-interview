a5a6925c5cfb425040735ca0e6f9e8c3135d8ce9
meraz007 <saifulmeraz897@gmail.com>
2024-09-10 21:51:10 +0600

    project setup

---
+++ alphabet-tile-interaction/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  
+    return (
+      <>
+        <div>
+            <h1 className='font-bold text-4xl'>Alhabet Tile Interaction</h1>
+        </div>
+      </>
+    )
+}
+
+export default App
