40b558cdcc7e0a0dcf0753ed1d21fd1648295e92
meraz007 <saifulmeraz897@gmail.com>
2024-09-10 17:32:30 +0600

    project setup

---
+++ recurive-partitioning/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='flex justify-center'>
+          <h1 className='font-bold text-4xl'>Recursive Partitioning</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
